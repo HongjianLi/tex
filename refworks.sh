c7a1372eb10ffba3941c2f1ad5cd52abf6d50514
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-02-25 10:59:59 +0800

    Updated refworks.sh to sed ~/Downloads/JackyLeeHongJian-RefList.txt

--- refworks.sh
+++ refworks.sh
@@ -1,1 +1,1 @@
-sed -e 's/[_&$]/\\&/g' -e 's/α/$\\alpha$/g' -e 's/β/$\\beta$/g' < JackyLeeHongJian-RefList.txt > refworks.bib
+sed -e 's/\r/\n/g' -e 's/[_&$]/\\&/g' -e 's/α/$\\alpha$/g' -e 's/β/$\\beta$/g' < ~/Downloads/JackyLeeHongJian-RefList.txt > refworks.bib
