59e31a0a87291a8b29b0fddc83fdd6606d59da47
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-10-10 15:49:40 +0800

    Changed %d to %s

--- rfcyscore/cv.R
+++ rfcyscore/cv.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 d=read.csv('cv.csv')
 for (p in 1:5) {
-	tiff(sprintf('cv-p%d.tiff',p),compress='lzw')
+	tiff(sprintf('cv-p%s.tiff',p),compress='lzw')
 	par(cex.lab=2,cex.axis=2,cex.main=2)
-	hist(d[d$Partition==p,]$pKd,xlab='pKd',main=sprintf('Partition %d',p),bg='transparent')
+	hist(d[d$Partition==p,]$pKd,xlab='pKd',main=sprintf('Partition %s',p),bg='transparent')
 	dev.off()
 }
