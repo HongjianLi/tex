913298935b0cb90f8a90e1992f6a429026def2d6
Jacky Lee <JackyLeeHongJian@Gmail.com>
2015-05-16 09:04:38 +0800

    Revised refworks.sh to auto update from the last id

--- refworks.sh
+++ refworks.sh
@@ -1,48 +1,57 @@
+id=$(tail refworks.bib | grep '@.*{' | cut -d{ -f2 | head -c -2)
 bibfields=(author title journal year volume number pages)
-suffix="},\n"
-for f in ~/Downloads/*.bib
-do
-	printf "\n%s{%s,\n" $(grep '@.*{' $f | cut -d{ -f1 | tr '[:upper:]' '[:lower:]') $(basename $f .bib)
-	for bibfield in ${bibfields[@]}
-	do
-		prefix="\t${bibfield}\t=\t{"
-		regexp="${bibfield[@]}[[:space:]]*=[[:space:]]*"
-		if [[ $bibfield == "title" ]]
-		then
-			printf "${prefix}{$(grep -i "${regexp}" $f | cut -d{ -f2 | cut -d} -f1)}${suffix}"
-		else
-			printf "${prefix}$(grep -i "${regexp}" $f | cut -d{ -f2 | cut -d} -f1)${suffix}"
-		fi
-	done
-	echo "}"
-done | sed -e 's/\r/\n/g' -e 's/[_&$]/\\&/g' -e 's/α/$\\alpha$/g' -e 's/β/$\\beta$/g' >> refworks.bib
 risfields=(AU TI JA PY VL IS [S,E]P)
-for f in ~/Downloads/*.ris
+suffix="},\n"
+while true
 do
-	entry=""
-	if [[ $(grep "TY[[:space:]]*-[[:space:]]*" $f | cut -d- -f2 | sed 's/[[:space:]]*//') == "JOUR" ]]
+	id=$((id+1))
+	f=$(find bib -name $id.*)
+	if [[ -z $f ]]
 	then
-		entry="article"
+		break
 	fi
-	printf "\n%s{%s,\n" "@${entry}" $(basename $f .ris)
-	for i in $(seq 0 ${#bibfields})
-	do
-		bibfield=${bibfields[i]}
-		risfield=${risfields[i]}
-		prefix="\t${bibfield}\t=\t{"
-		regexp="${risfield[@]}[[:space:]]*-[[:space:]]*"
-		if [[ $bibfield == "title" ]]
+	case ${f##*.} in
+	bib)
+		printf "\n%s{%s,\n" $(grep '@.*{' $f | cut -d{ -f1 | tr '[:upper:]' '[:lower:]') $id
+		for bibfield in ${bibfields[@]}
+		do
+			prefix="\t${bibfield}\t=\t{"
+			regexp="${bibfield[@]}[[:space:]]*=[[:space:]]*"
+			if [[ $bibfield == "title" ]]
+			then
+				printf "${prefix}{$(grep -i "${regexp}" $f | cut -d{ -f2 | cut -d} -f1)}${suffix}"
+			else
+				printf "${prefix}$(grep -i "${regexp}" $f | cut -d{ -f2 | cut -d} -f1)${suffix}"
+			fi
+		done
+	;;
+	ris)
+		entry=""
+		if [[ $(grep "TY[[:space:]]*-[[:space:]]*" $f | cut -d- -f2 | sed 's/[[:space:]]*//') == "JOUR" ]]
 		then
-			printf "${prefix}{$(grep "${regexp}" $f | cut -d- -f2 | sed 's/[[:space:]]*//')}${suffix}"
-		elif [[ $bibfield == "author" ]]
-		then
-			printf "${prefix}$(grep "${regexp}" $f | cut -d- -f2 | sed 's/[[:space:]]*//' | head -c -1 | tr '\n' '=' | sed 's/=/ and /g')${suffix}"
-		elif [[ $bibfield == "pages" ]]
-		then
-			printf "${prefix}$(grep "${regexp}" $f | cut -d- -f2 | sed 's/[[:space:]]*//' | head -c -1 | tr '\n' '-')${suffix}"
-		else
-			printf "${prefix}$(grep "${regexp}" $f | cut -d- -f2 | sed 's/[[:space:]]*//')${suffix}"
+			entry="article"
 		fi
-	done
+		printf "\n%s{%s,\n" "@${entry}" $id
+		for i in $(seq 0 ${#bibfields})
+		do
+			bibfield=${bibfields[i]}
+			risfield=${risfields[i]}
+			prefix="\t${bibfield}\t=\t{"
+			regexp="${risfield[@]}[[:space:]]*-[[:space:]]*"
+			if [[ $bibfield == "title" ]]
+			then
+				printf "${prefix}{$(grep "${regexp}" $f | cut -d- -f2 | sed 's/[[:space:]]*//')}${suffix}"
+			elif [[ $bibfield == "author" ]]
+			then
+				printf "${prefix}$(grep "${regexp}" $f | cut -d- -f2 | sed 's/[[:space:]]*//' | head -c -1 | tr '\n' '=' | sed 's/=/ and /g')${suffix}"
+			elif [[ $bibfield == "pages" ]]
+			then
+				printf "${prefix}$(grep "${regexp}" $f | cut -d- -f2 | sed 's/[[:space:]]*//' | head -c -1 | tr '\n' '-')${suffix}"
+			else
+				printf "${prefix}$(grep "${regexp}" $f | cut -d- -f2 | sed 's/[[:space:]]*//')${suffix}"
+			fi
+		done
+	;;
+	esac
 	echo "}"
 done | sed -e 's/\r/\n/g' -e 's/[_&$]/\\&/g' -e 's/α/$\\alpha$/g' -e 's/β/$\\beta$/g' >> refworks.bib
